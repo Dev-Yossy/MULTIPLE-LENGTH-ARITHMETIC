3467ea34d1cc2b2a01c8c9114759c5e6c538e7ca
Dev-G_HaC <kazumaj439@gmail.com>
2019-11-21 16:10:04 +0900

    [Update]

--- MULTIPLE-LENGTH-ARITHMETIC/MLA.c
+++ MULTIPLE-LENGTH-ARITHMETIC/MLA.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include "mulprec.h"
 
+void check_setInt(struct NUMBER* a, int roop);
+
 int main(void)
 {
 	srand((unsigned)time(NULL));
@@ -12,12 +14,14 @@ int main(void)
 	clearByZero(&a);
 	clearByZero(&b);
 
+	/*
+	
 	setRnd(&a, 3);
 
 	printf("a = ");
 	dispNumber(&a);
 	putchar('\n');
-
+	
 	mulBy10(&a, &b);
 
 	printf("b = ");
@@ -35,6 +39,37 @@ int main(void)
 	dispNumber(&a);
 	putchar('\n');
 
+	*/
+
+	check_setInt(&a, 10000000);
+
 	return 0;
 }
 
+
+void check_setInt(struct NUMBER *a, int roop)
+{
+	int i, roopDiv100 = roop / 100;
+
+	printf("\r%4d %%\r", 0);
+
+	for (i = 0; i < roop; i++)
+	{
+		int x = rand();
+		setInt(a, x);
+
+		if (check(a, x+1))
+		{
+			printf("a = ");
+			dispNumber(a);
+			putchar('\n');
+			printf("x = %d\n", x);
+		}
+
+		if ((i + 1) % roopDiv100 == 0)
+		{
+			printf("\r%4d %%\r", (i + 1) / roopDiv100);
+		}
+	}
+	printf("\r%4d %% --- Done !\n", 100);
+}
