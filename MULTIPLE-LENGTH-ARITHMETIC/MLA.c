0306dace19360442266e9bfb13017cb0a07a89c4
G_HaC <kazumaj439@gmail.com>
2020-01-12 01:39:00 +0900

    [Update] Slight Adjustment

--- MULTIPLE-LENGTH-ARITHMETIC/MLA.c
+++ MULTIPLE-LENGTH-ARITHMETIC/MLA.c
@@ -3,75 +3,235 @@
 #include <stdlib.h>
 #include "mulprec.h"
 
-
-
 int main(void)
 {
 	srand((unsigned)time(NULL));
 
+	int errorCode;
 	struct NUMBER a, b, c, d, e;
-
-	int roop_add = 100000;
-	printf("roop function \'add\' %d times.\n", roop_add);
-	printf("--- Start ---\n");
-	RoopFunction_Cul(add, roop_add, 10, None);
-	printf("--- Done ! ---\n");
-
-	int roop_mul = 100000;
-	printf("roop function \'mul\' %d times.\n", roop_mul);
-	printf("--- Start ---\n");
-	RoopFunction_Cul(multiple, roop_mul, 10, None);
-	printf("--- Done ! ---\n");
-
-	int roop_dev_mul = 100000;
-	printf("roop function \'Dev_mul\' %d times.\n", roop_dev_mul);
-	printf("--- Start ---\n");
-	RoopFunction_Cul(Dev_multiple, roop_dev_mul, 10, None);
-	printf("--- Done ! ---\n");
-
-	int roop_div = 100000;
-	printf("roop function \'div\' %d times.\n", roop_div);
-	printf("--- Start ---\n");
-	RoopFunction_Cul(divide, roop_div, 10, All);
-	printf("--- Done ! ---\n");
-
-
-
-	/*
 	clearByZero(&a);
 	clearByZero(&b);
 	clearByZero(&c);
-	setInt(&a, 12345);
-	setInt(&b, -2);
-	printf("a = ");	dispNumber(&a);	putchar('\n');
-	printf("b = ");	dispNumber(&b);	putchar('\n');
-	multiple(&a, &b, &c);
-	printf("c(Nor) = ");	dispNumber(&c);	putchar('\n');
-	Dev_multiple(&a, &b, &c);
-	printf("c(Dev) = ");	dispNumber(&c);	putchar('\n');
-	*/
-
-	/*
-	setInt(&a, -37);
-	setInt(&b, -6);
-	clearByZero(&c);
 	clearByZero(&d);
-	printf("a = ");	dispNumber(&a);	putchar('\n');
-	printf("b = ");	dispNumber(&b);	putchar('\n');
-	switch (divide(&a, &b, &c, &d))
+	clearByZero(&e);
+
+
+	//‰ÁŽZ********************************************************
+	if (0)
 	{
-	case 0:
-		break;
-	case -1:
-		printf("divide by zero error.\n");
-		break;
-	case 1:
-		printf("uncollectable error.\n");
-		break;
+		int roop_add = 0;//100000;
+		printf("roop function \'add\' %d times.\n", roop_add);
+		printf("--- Start ---\n");
+		RoopFunction_ASM(add, roop_add, 10, None);
+		printf("--- Done ! ---\n");
 	}
-	printf("c = ");	dispNumber(&c);	putchar('\n');
-	printf("d = ");	dispNumber(&d);	putchar('\n');
-	*/
+
+	//æŽZ********************************************************
+	if (0)
+	{
+		int roop_mul = 100000;
+		printf("roop function \'mul\' %d times.\n", roop_mul);
+		printf("--- Start ---\n");
+		RoopFunction_ASM(multiple, roop_mul, 10, None);
+		printf("--- Done ! ---\n");
+	}
+
+	//æŽZDev*****************************************************
+	if (0)
+	{
+		int roop_dev_mul = 100000;
+		printf("roop function \'Dev_mul\' %d times.\n", roop_dev_mul);
+		printf("--- Start ---\n");
+		RoopFunction_ASM(Dev_multiple, roop_dev_mul, 10, None);
+		printf("--- Done ! ---\n");
+	}
+
+	//œŽZ********************************************************
+	if (0)
+	{
+		int roop_div = 100000;
+		printf("roop function \'div\' %d times.\n", roop_div);
+		printf("--- Start ---\n");
+		RoopFunction_D(divide, roop_div, 10, All);
+		printf("--- Done ! ---\n");
+	}
+
+	//æŽZ********************************************************
+	if (1)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+
+		//³í“®ì
+		printf("\næŽZ-³í“®ì----------\n");
+		setInt(&a, 250);
+		setInt(&b, 33);
+		if (errorCode = multiple(&a, &b, &c))
+		{
+			printf("func > multiple - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a         = ");		dispNumber(&a);	putchar('\n');
+			printf("b         = ");		dispNumber(&b);	putchar('\n');
+			printf("c = a * b = ");		dispNumber(&c);	putchar('\n');
+		}
+
+		//overflow
+		printf("\næŽZ-ƒGƒ‰[(overflow)----------\n");
+		setRnd(&a, 18);
+		setInt(&b, 999999999);
+		if (errorCode = multiple(&a, &b, &c))
+		{
+			printf("func > multiple - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a =         ");		dispNumber(&a);	putchar('\n');
+			printf("b =         ");		dispNumber(&b);	putchar('\n');
+			printf("c = a * b = ");		dispNumber(&c);	putchar('\n');
+		}
+	}
+
+	//™ŽZ********************************************************
+	if (1)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+		clearByZero(&d);
+
+		//³í“®ì
+		printf("\nœŽZ-³í“®ì----------\n");
+		setInt(&a, 1000);
+		setInt(&b, 33);
+		if (errorCode = divide(&a, &b, &c, &d))
+		{
+			printf("func > divide - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a         = ");		dispNumber(&a);	putchar('\n');
+			printf("b         = ");		dispNumber(&b);	putchar('\n');
+			printf("c = a / b = ");		dispNumber(&c);	putchar('\n');
+			printf("d = a %% b = ");	dispNumber(&d);	putchar('\n');
+		}
+
+		//b = 0
+		printf("\nœŽZ-ƒGƒ‰[(b=0)----------\n");
+		setInt(&a, 1000);
+		setInt(&b, 0);
+		if (errorCode = divide(&a, &b, &c, &d))
+		{
+			printf("func > divide - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a =         ");		dispNumber(&a);	putchar('\n');
+			printf("b =         ");		dispNumber(&b);	putchar('\n');
+			printf("c = a / b = ");		dispNumber(&c);	putchar('\n');
+			printf("d = a %% b = ");	dispNumber(&d);	putchar('\n');
+		}
+	}
+
+	//—Ýæ********************************************************
+	if (1)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+
+		//³í“®ì
+		printf("\n—Ýæ-³í“®ì----------\n");
+		setInt(&a, 3);
+		setInt(&b, 5);
+		if (errorCode = power(&a, &b, &c))
+		{
+			printf("func > power - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a       = ");	dispNumber(&a);	putchar('\n');
+			printf("b       = ");	dispNumber(&b);	putchar('\n');
+			printf("c = a^b = ");	dispNumber(&c);	putchar('\n');
+		}
+
+		//b < 0
+		printf("\n—Ýæ-ƒGƒ‰[(b < 0)----------\n");
+		setInt(&b, -3);
+		if (errorCode = power(&a, &b, &c))
+		{
+			printf("func > power - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a       = ");	dispNumber(&a);	putchar('\n');
+			printf("b       = ");	dispNumber(&b);	putchar('\n');
+			printf("c = a^b = ");	dispNumber(&c);	putchar('\n');
+		}
+
+		//overflow
+		printf("\n—Ýæ-ƒGƒ‰[(overflow)----------\n");
+		setInt(&a, 1000);
+		setInt(&b, 10);
+		if (errorCode = power(&a, &b, &c))
+		{
+			printf("func > power - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a       = ");	dispNumber(&a);	putchar('\n');
+			printf("b       = ");	dispNumber(&b);	putchar('\n');
+			printf("c = a^b = ");	dispNumber(&c);	putchar('\n');
+		}
+	}
+
+	//ŠKæ********************************************************
+	if (1)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+
+		//³í“®ì
+		printf("\nŠKæ-³í“®ì----------\n");
+		setInt(&a, 5);
+		if (errorCode = factorial(&a, &b))
+		{
+			printf("func > factorial - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a      = ");	dispNumber(&a);	putchar('\n');
+			printf("b = a! = ");	dispNumber(&b);	putchar('\n');
+		}
+
+		//overflow
+		printf("\n—Ýæ - ƒGƒ‰[(overflow)----------\n");
+		setInt(&a, 1000);
+		if (errorCode = factorial(&a, &b))
+		{
+			printf("func > factorial - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a      = ");	dispNumber(&a);	putchar('\n');
+			printf("b = a! = ");	dispNumber(&b);	putchar('\n');
+		}
+
+		//a<0
+		printf("\n—Ýæ - ƒGƒ‰[(a<0)----------\n");
+		setInt(&a, -3);
+		if (errorCode = factorial(&a, &b))
+		{
+			printf("func > factorial - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a      = ");	dispNumber(&a);	putchar('\n');
+			printf("b = a! = ");	dispNumber(&b);	putchar('\n');
+		}
+	}
 
 	return 0;
 }
