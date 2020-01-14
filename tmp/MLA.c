7c0c7ea7a8343dddc6469e3bd2bb166193e76ee2
G_HaC <kazumaj439@gmail.com>
2020-01-14 17:10:05 +0900

    [Update] Fixed a function func > main % It is for VSCode %

--- tmp/MLA.c
+++ tmp/MLA.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include "mulprec.h"
 
-
+//rand -> random にすること
 
 int main(void)
 {
 	srand((unsigned)time(NULL));
 
+	int errorCode;
 	struct NUMBER a, b, c, d, e;
 	clearByZero(&a);
 	clearByZero(&b);
@@ -16,82 +17,296 @@ int main(void)
 	clearByZero(&d);
 	clearByZero(&e);
 
+	int RoopSetting_Roop = 0;//1000;
+	int RoopSetting_RndLen = 30;
 
-	//加算***********************************************************
-	int roop_add = 0;//100000;
-	printf("roop function \'add\' %d times.\n", roop_add);
-	printf("--- Start ---\n");
-	RoopFunction_ASM(add, roop_add, 10, None);
-	printf("--- Done ! ---\n");
-
-	//乗算***********************************************************
-	int roop_mul = 0;//100000;
-	printf("roop function \'mul\' %d times.\n", roop_mul);
-	printf("--- Start ---\n");
-	RoopFunction_ASM(multiple, roop_mul, 10, None);
-	printf("--- Done ! ---\n");
-
-	//乗算***********************************************************
-	int roop_dev_mul = 0;//100000;
-	printf("roop function \'Dev_mul\' %d times.\n", roop_dev_mul);
-	printf("--- Start ---\n");
-	RoopFunction_ASM(Dev_multiple, roop_dev_mul, 10, None);
-	printf("--- Done ! ---\n");
-
-	//除算***********************************************************
-	int roop_div = 0;//100000;
-	printf("roop function \'div\' %d times.\n", roop_div);
-	printf("--- Start ---\n");
-	RoopFunction_D(divide, roop_div, 10, All);
-	printf("--- Done ! ---\n");
-
-
-
-	/*
-	clearByZero(&a);
-	clearByZero(&b);
-	clearByZero(&c);
-	setInt(&a, 12345);
-	setInt(&b, -2);
-	printf("a = ");	dispNumber(&a);	putchar('\n');
-	printf("b = ");	dispNumber(&b);	putchar('\n');
-	multiple(&a, &b, &c);
-	printf("c(Nor) = ");	dispNumber(&c);	putchar('\n');
-	Dev_multiple(&a, &b, &c);
-	printf("c(Dev) = ");	dispNumber(&c);	putchar('\n');
-	*/
-
-	/*
-	setInt(&a, -37);
-	setInt(&b, -6);
-	clearByZero(&c);
-	clearByZero(&d);
-	printf("a = ");	dispNumber(&a);	putchar('\n');
-	printf("b = ");	dispNumber(&b);	putchar('\n');
-	switch (divide(&a, &b, &c, &d))
+	//加算ループ**************************************************
+	if (1)
 	{
-	case 0:
-		break;
-	case -1:
-		printf("divide by zero error.\n");
-		break;
-	case 1:
-		printf("uncollectable error.\n");
-		break;
+		int roop_add = RoopSetting_Roop;
+		printf("roop function \'add\' %d times.\n", roop_add);
+		printf("--- Start ---\n");
+		clockStart();
+		RoopFunction_ASM(add, roop_add, RoopSetting_RndLen, None);
+		unsigned int tmp = clockStop();
+		printf("--- Done ! ---\n");
+		printf("--- time = %d [ms]", tmp);
 	}
-	printf("c = ");	dispNumber(&c);	putchar('\n');
-	printf("d = ");	dispNumber(&d);	putchar('\n');
-	*/
+
+	//乗算ループ**************************************************
+	if (1)
+	{
+		int roop_mul = RoopSetting_Roop;
+		printf("roop function \'mul\' %d times.\n", roop_mul);
+		printf("--- Start ---\n");
+		clockStart();
+		RoopFunction_ASM(multiple, roop_mul, RoopSetting_RndLen, None);
+		unsigned int tmp = clockStop();
+		printf("--- Done ! ---\n");
+		printf("--- time = %d [ms]", tmp);
+	}
+
+	//乗算Devループ***********************************************
+	if (1)
+	{
+		int roop_dev_mul = RoopSetting_Roop;
+		printf("roop function \'Dev_mul\' %d times.\n", roop_dev_mul);
+		printf("--- Start ---\n");
+		clockStart();
+		RoopFunction_ASM(Dev_multiple, roop_dev_mul, RoopSetting_RndLen, None);
+		unsigned int tmp = clockStop();
+		printf("--- Done ! ---\n");
+		printf("--- time = %d [ms]", tmp);
+	}
+
+	//除算ループ**************************************************
+	if (1)
+	{
+		int roop_div = RoopSetting_Roop;
+		printf("roop function \'div\' %d times.\n", roop_div);
+		printf("--- Start ---\n");
+		clockStart();
+		RoopFunction_D(divide, roop_div, RoopSetting_RndLen, None);
+		unsigned int tmp = clockStop();
+		printf("--- Done ! ---\n");
+		printf("--- time = %d [ms]", tmp);
+	}
+
+	//乗算********************************************************
+	if (0)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+
+		//正常動作
+		printf("\n乗算-正常動作----------\n");
+		setInt(&a, 250);
+		setInt(&b, 33);
+		errorCode = multiple(&a, &b, &c);
+		if (errorCode)
+		{
+			printf("func > multiple - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a         = ");		dispNumber(&a);	putchar('\n');
+			printf("b         = ");		dispNumber(&b);	putchar('\n');
+			printf("c = a * b = ");		dispNumber(&c);	putchar('\n');
+		}
+
+		//overflow
+		printf("\n乗算-エラー(overflow)----------\n");
+		setRnd(&a, 18);
+		setInt(&b, 999999999);
+		errorCode = multiple(&a, &b, &c);
+		if (errorCode)
+		{
+			printf("func > multiple - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a =         ");		dispNumber(&a);	putchar('\n');
+			printf("b =         ");		dispNumber(&b);	putchar('\n');
+			printf("c = a * b = ");		dispNumber(&c);	putchar('\n');
+		}
+	}
+
+	//徐算********************************************************
+	if (1)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+		clearByZero(&d);
+
+		//正常動作
+		printf("\n除算-正常動作----------\n");
+		setInt(&a, 1323420041);
+		a.n[9] = 9;
+		setInt(&b, 58496432);
+		errorCode = Dev_divide(&a, &b, &c, &d);
+		if (errorCode)
+		{
+			printf("func > divide - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a         = ");		dispNumber(&a);	putchar('\n');
+			printf("b         = ");		dispNumber(&b);	putchar('\n');
+			printf("c = a / b = ");		dispNumber(&c);	putchar('\n');
+			printf("d = a %% b = ");	dispNumber(&d);	putchar('\n');
+		}
+
+		//b = 0
+		printf("\n除算-エラー(b=0)----------\n");
+		setInt(&a, 1000);
+		setInt(&b, 0);
+		errorCode = divide(&a, &b, &c, &d);
+		if (errorCode)
+		{
+			printf("func > divide - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a =         ");		dispNumber(&a);	putchar('\n');
+			printf("b =         ");		dispNumber(&b);	putchar('\n');
+			printf("c = a / b = ");		dispNumber(&c);	putchar('\n');
+			printf("d = a %% b = ");	dispNumber(&d);	putchar('\n');
+		}
+	}
+
+	//累乗********************************************************
+	if (0)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+
+		//正常動作
+		printf("\n累乗-正常動作----------\n");
+		setInt(&a, 3);
+		setInt(&b, 5);
+		errorCode = power(&a, &b, &c);
+		if (errorCode)
+		{
+			printf("func > power - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a       = ");	dispNumber(&a);	putchar('\n');
+			printf("b       = ");	dispNumber(&b);	putchar('\n');
+			printf("c = a^b = ");	dispNumber(&c);	putchar('\n');
+		}
+
+		//b < 0
+		printf("\n累乗-エラー(b < 0)----------\n");
+		setInt(&b, -3);
+		errorCode = power(&a, &b, &c);
+		if (errorCode)
+		{
+			printf("func > power - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a       = ");	dispNumber(&a);	putchar('\n');
+			printf("b       = ");	dispNumber(&b);	putchar('\n');
+			printf("c = a^b = ");	dispNumber(&c);	putchar('\n');
+		}
+
+		//overflow
+		printf("\n累乗-エラー(overflow)----------\n");
+		setInt(&a, 1000);
+		setInt(&b, 10);
+		errorCode = power(&a, &b, &c);
+		if (errorCode)
+		{
+			printf("func > power - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a       = ");	dispNumber(&a);	putchar('\n');
+			printf("b       = ");	dispNumber(&b);	putchar('\n');
+			printf("c = a^b = ");	dispNumber(&c);	putchar('\n');
+		}
+	}
 
 	//階乗********************************************************
-	setInt(&a, 5);
-	if(factorial(&a, &b))
-		printf("Failed ...\n");
-	else
-		printf("Success !\n");
-	
-	printf("a = ");	dispNumber(&a);	putchar('\n');
-	printf("a! = ");	dispNumber(&b);	putchar('\n');
+	if (0)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+
+		//正常動作
+		printf("\n階乗-正常動作----------\n");
+		setInt(&a, 5);
+		errorCode = factorial(&a, &b);
+		if (errorCode)
+		{
+			printf("func > factorial - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a      = ");	dispNumber(&a);	putchar('\n');
+			printf("b = a! = ");	dispNumber(&b);	putchar('\n');
+		}
+
+		//overflow
+		printf("\n累乗 - エラー(overflow)----------\n");
+		setInt(&a, 1000);
+		errorCode = factorial(&a, &b);
+		if (errorCode)
+		{
+			printf("func > factorial - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a      = ");	dispNumber(&a);	putchar('\n');
+			printf("b = a! = ");	dispNumber(&b);	putchar('\n');
+		}
+
+		//a<0
+		printf("\n累乗 - エラー(a<0)----------\n");
+		setInt(&a, -3);
+		errorCode = factorial(&a, &b);
+		if (errorCode)
+		{
+			printf("func > factorial - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a      = ");	dispNumber(&a);	putchar('\n');
+			printf("b = a! = ");	dispNumber(&b);	putchar('\n');
+		}
+	}
+
+	//最大公約数***************************************************
+	if (0)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+
+		//正常動作
+		printf("\n最大公約数-正常動作----------\n");
+		setInt(&a, 1357911);
+		setInt(&b, 246810);
+		errorCode = gcd(&a, &b, &c);
+		if (errorCode)
+		{
+			printf("func > gcd - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a            = ");	dispNumber(&a);	putchar('\n');
+			printf("b            = ");	dispNumber(&b);	putchar('\n');
+			printf("c = gcd(a,b) = ");	dispNumber(&c);	putchar('\n');
+		}
+	}
+
+	//最小公倍数***************************************************
+	if (0)
+	{
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+
+		//正常動作
+		printf("\n最小公倍数-正常動作----------\n");
+		setInt(&a, 98765);
+		setInt(&b, 43210);
+		errorCode = lcm(&a, &b, &c);
+		if (errorCode)
+		{
+			printf("func > lcm - failed.  code > %d\n", errorCode);
+		}
+		else
+		{
+			printf("a            = ");	dispNumber(&a);	putchar('\n');
+			printf("b            = ");	dispNumber(&b);	putchar('\n');
+			printf("c = lcm(a,b) = ");	dispNumber(&c);	putchar('\n');
+		}
+	}
 
 	return 0;
 }
